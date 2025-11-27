fef58459d8619be98ec263f7c65d4747417a0f4c
Christopher Murray <christopher.murra2@ucalgary.ca>
2025-11-27 02:48:03 -0700

    fix: correct import of BENCHMARK_AVAILABLE in cli/__init__.py

--- src/parquetframe/cli/__init__.py
+++ src/parquetframe/cli/__init__.py
@@ -2,8 +2,8 @@
 CLI utilities for ParquetFrame.
 """
 
-from ..exceptions import BENCHMARK_AVAILABLE, check_dependencies
-from .commands import console, main
+from ..exceptions import check_dependencies
+from .commands import BENCHMARK_AVAILABLE, console, main
 from .repl import start_basic_repl, start_repl
 
 __all__ = [
